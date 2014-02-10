df16be77fd74142126369d9af78affd09602689c
MarZl <MarZl@users.noreply.github.com>
2014-02-10 22:24:10 +0100

    custom notifications via input

--- src/main/java/de/codeset/gwt/client/notification/sample/NotificationForm.java
+++ src/main/java/de/codeset/gwt/client/notification/sample/NotificationForm.java
@@ -2,48 +2,148 @@ package de.codeset.gwt.client.notification.sample;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.LongBox;
 import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
 
 import de.codeset.gwt.client.notification.Notification;
 import de.codeset.gwt.client.notification.Notification.NotificationOptions;
+import de.codeset.gwt.client.notification.eventing.click.NotificationClickEvent;
+import de.codeset.gwt.client.notification.eventing.click.NotificationClickHandler;
+import de.codeset.gwt.client.notification.eventing.close.NotificationCloseEvent;
+import de.codeset.gwt.client.notification.eventing.close.NotificationCloseHandler;
+import de.codeset.gwt.client.notification.eventing.error.NotificationErrorEvent;
+import de.codeset.gwt.client.notification.eventing.error.NotificationErrorHandler;
+import de.codeset.gwt.client.notification.eventing.show.NotificationShowEvent;
+import de.codeset.gwt.client.notification.eventing.show.NotificationShowHandler;
 
+/**
+ * @author MarZl
+ * 
+ * @see <a href="https://github.com/MarZl/">https://github.com/MarZl/</a>
+ * 
+ */
+public class NotificationForm extends FlowPanel implements ClickHandler {
 
-public class NotificationForm extends FlowPanel implements ClickHandler{
-
-
-	private TextArea body;
-	private TextBox title;
-
+	private TextBox inputTitle;
+	private ListBox inputDir;
+	private CheckBox inputEnableHandlers;
+	private TextArea inputBody;
+	private LongBox inputDuration;
+	private TextBox inputIcon;
 
 	public NotificationForm() {
 		init();
 	}
 
-
 	private void init() {
-		title = new TextBox();
-		add(title);
 
-		body = new TextArea();
-		add(body);
+		Label lblTitle = new Label("title:");
+		add(lblTitle);
+
+		inputTitle = new TextBox();
+		add(inputTitle);
 		
+		Label lblIcon = new Label("icon [url]:");
+		add(lblIcon);
+
+		inputIcon = new TextBox();
+		add(inputIcon);
+		
+		Label lblDir = new Label("direction:");
+		add(lblDir);
+
+		inputDir = new ListBox();
+		inputDir.addItem("auto");
+		inputDir.addItem("ltr");
+		inputDir.addItem("rtl");
+		add(inputDir);
+
+		Label lblEnableHandlers = new Label("enable handlers:");
+		add(lblEnableHandlers);
+
+		inputEnableHandlers = new CheckBox();
+		add(inputEnableHandlers);
+
+		Label lblBody = new Label("body:");
+		add(lblBody);
+
+		inputBody = new TextArea();
+		add(inputBody);
+
+		Label lblDuration = new Label("duration [ms]:");
+		add(lblDuration);
+
+		inputDuration = new LongBox();
+		inputDuration.setText("5000");
+		add(inputDuration);
+
 		Button showBtn = new Button("create");
 		showBtn.addClickHandler(this);
 		add(showBtn);
-		
-		
 	}
-
 
 	@Override
 	public void onClick(ClickEvent event) {
-		String titleMsg = title.getText();
-		String bodyMsg = body.getText();
+		String title = inputTitle.getText();
+		String dir = inputDir.getValue(inputDir.getSelectedIndex());
+		String body = inputBody.getText();
+		String icon = inputIcon.getText();
+
+		final Notification notification = Notification.createIfSupported(title, NotificationOptions.create().body(body).dir(dir).icon(icon));
+
+		if (inputEnableHandlers.getValue()) {
+			notification.addCloseHandler(new NotificationCloseHandler() {
+
+				@Override
+				public void onClose(NotificationCloseEvent event) {
+					Window.alert("closed");
+				}
+			});
+			notification.addClickHandler(new NotificationClickHandler() {
+
+				@Override
+				public void onClick(NotificationClickEvent event) {
+					Window.alert("click");
+				}
+			});
+			notification.addErrorHandler(new NotificationErrorHandler() {
+
+				@Override
+				public void onError(NotificationErrorEvent event) {
+					Window.alert("error");
+				}
+			});
+			notification.addShowHandler(new NotificationShowHandler() {
+
+				@Override
+				public void onShow(NotificationShowEvent event) {
+					Window.alert("show");
+				}
+			});
+		}
 		
-		Notification.createIfSupported(titleMsg, NotificationOptions.create().body(bodyMsg));
+		try {
+			int duration = Integer.parseInt(inputDuration.getText());
+
+			new Timer() {
+
+				@Override
+				public void run() {
+					notification.close();
+				}
+			}.schedule(duration);
+
+		} catch (Exception e) {
+			Window.alert("error parsing duration");
+		}
 	}
 
 }
--- src/main/java/de/codeset/gwt/client/notification/sample/NotificationTest.java
+++ src/main/java/de/codeset/gwt/client/notification/sample/NotificationTest.java
@@ -5,20 +5,12 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 
 import de.codeset.gwt.client.notification.Notification;
-import de.codeset.gwt.client.notification.NotificationPermission;
-import de.codeset.gwt.client.notification.Notification.NotificationOptions;
 import de.codeset.gwt.client.notification.Notification.NotificationPermissionCallback;
-import de.codeset.gwt.client.notification.eventing.click.NotificationClickEvent;
-import de.codeset.gwt.client.notification.eventing.click.NotificationClickHandler;
-import de.codeset.gwt.client.notification.eventing.close.NotificationCloseEvent;
-import de.codeset.gwt.client.notification.eventing.close.NotificationCloseHandler;
-import de.codeset.gwt.client.notification.eventing.error.NotificationErrorEvent;
-import de.codeset.gwt.client.notification.eventing.error.NotificationErrorHandler;
-import de.codeset.gwt.client.notification.eventing.show.NotificationShowEvent;
-import de.codeset.gwt.client.notification.eventing.show.NotificationShowHandler;
+import de.codeset.gwt.client.notification.NotificationPermission;
 
 /**
  * @author MarZl 
@@ -26,7 +18,7 @@ import de.codeset.gwt.client.notification.eventing.show.NotificationShowHandler;
  * @see <a href="https://github.com/MarZl/">https://github.com/MarZl/</a>
  * 
  */ 
-public class NotificationTest implements EntryPoint {
+public class NotificationTest extends FlowPanel implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
@@ -38,8 +30,17 @@ public class NotificationTest implements EntryPoint {
 
 	}
 
+	
+	public NotificationTest() {
+		setStyleName("notificationTest");
+	}
+	
+	
 	private void init() {
-		Button btn = new Button("Request");
+
+		RootPanel.get().add(this);
+		
+		Button btn = new Button("requestPermission");
 		btn.addClickHandler(new ClickHandler() {
 
 			@Override
@@ -54,7 +55,7 @@ public class NotificationTest implements EntryPoint {
 				});
 			}
 		});
-		RootPanel.get().add(btn);
+		add(btn);
 
 
 		
@@ -73,56 +74,9 @@ public class NotificationTest implements EntryPoint {
 				});
 			}
 		});
-		RootPanel.get().add(btn2);
+		add(btn2);
 
-		
-		
-		Button btn3 = new Button("create");
-		btn3.addClickHandler(new ClickHandler() {
-
-			@Override
-			public void onClick(ClickEvent event) {
-				Notification.requestPermission(new NotificationPermissionCallback() {
-
-					@Override
-					public void call(NotificationPermission permission) {
-						Notification noti = Notification.createIfSupported("Title", NotificationOptions.create().body("body").dir("rtl").icon("http://devisen-handeln.org/forex-news/wp-content/uploads/2013/11/twitter-ipo.png"));
-						noti.addCloseHandler(new NotificationCloseHandler() {
-
-							@Override
-							public void onClose(NotificationCloseEvent event) {
-								Window.alert("closed");
-							}
-						});
-						noti.addClickHandler(new NotificationClickHandler() {
-
-							@Override
-							public void onClick(NotificationClickEvent event) {
-								Window.alert("click");
-							}
-						});
-						noti.addErrorHandler(new NotificationErrorHandler() {
-
-							@Override
-							public void onError(NotificationErrorEvent event) {
-								Window.alert("error");
-							}
-						});
-						noti.addShowHandler(new NotificationShowHandler() {
-
-							@Override
-							public void onShow(NotificationShowEvent event) {
-								Window.alert("show");
-							}
-						});
-					}
-				});
-			}
-		});
-
-		RootPanel.get().add(btn3);
-		
-		RootPanel.get().add(new NotificationForm());
+		add(new NotificationForm());
 	}
 
 }
